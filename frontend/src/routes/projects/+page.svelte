ff1455e2a7e4c552aa1616fd1a813bac97c4cbcf
Nirmal Ariyathilake <nirmal.ariyathilake@gmail.com>
2023-01-21 06:35:03 +0530

    Frontend : Projects placeholder page

---
+++ frontend/src/routes/projects/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>Nirmal Code</title>
+</svelte:head>
+
+<div class="flex min-h-screen w-full flex-col items-center justify-center font-body">
+	<main class="flex grow flex-col items-center justify-center">
+		<p class="text-2xl font-semibold text-primary">PAGE UNDER MAINTENANCE</p>
+		<a href={'/'} class="btn-link btn mt-10 text-xl"> Go back Home </a>
+	</main>
+</div>
