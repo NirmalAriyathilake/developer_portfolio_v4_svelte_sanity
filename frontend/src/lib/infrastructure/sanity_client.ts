1a6c54fdf6b0586d707bedc294be5beee630b8c7
Nirmal Ariyathilake <nirmal.ariyathilake@gmail.com>
2023-01-21 15:14:35 +0530

    Frontend : Sanity authorize requests

--- frontend/src/lib/infrastructure/sanity_client.ts
+++ frontend/src/lib/infrastructure/sanity_client.ts
@@ -3,6 +3,7 @@ import sanityClient from '@sanity/client';
 import {
 	SANITY_STUDIO_DATASET,
 	SANITY_STUDIO_PROJECT_ID,
+	SANITY_STUDIO_TOKEN,
 	SANITY_STUDIO_USE_CDN
 } from '$env/static/private';
 
@@ -10,5 +11,6 @@ export const client = sanityClient({
 	projectId: SANITY_STUDIO_PROJECT_ID,
 	dataset: SANITY_STUDIO_DATASET,
 	apiVersion: '2023-01-16',
-	useCdn: SANITY_STUDIO_USE_CDN == 'true'
+	useCdn: SANITY_STUDIO_USE_CDN == 'true',
+	token: SANITY_STUDIO_TOKEN
 });
