06ee0e179fb0cd7ae98fcf5c3abe79a0f832d08f
Nirmal Ariyathilake <nirmal.ariyathilake@gmail.com>
2023-01-22 23:47:29 +0530

    Frontend : Services section responsive

--- frontend/src/lib/sections/home/services_section.svelte
+++ frontend/src/lib/sections/home/services_section.svelte
@@ -9,11 +9,11 @@
 
 {#if data.length > 0}
 	<div
-		class={`flex w-full scroll-m-24 flex-col items-start justify-start px-10 py-10 ${className}`}
+		class={`section flex w-full flex-col items-start justify-start px-10 py-10 ${className}`}
 		id="services"
 	>
 		<SectionTitle title="My Services" />
-		<div class="mt-28 grid grid-cols-3 grid-rows-1 gap-5">
+		<div class="flex flex-col gap-10 lg:flex-row">
 			{#each data as service}
 				<ServiceCard data={service} />
 			{/each}
