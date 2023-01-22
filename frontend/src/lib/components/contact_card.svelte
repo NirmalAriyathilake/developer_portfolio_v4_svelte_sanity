c8b6801b2a9de1bab5dcf6664afc96a148ac7d54
Nirmal Ariyathilake <nirmal.ariyathilake@gmail.com>
2023-01-23 02:40:50 +0530

    Frontend : Contact card minor fix

--- frontend/src/lib/components/contact_card.svelte
+++ frontend/src/lib/components/contact_card.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <a
-	class={`flex flex-col items-center justify-center rounded-r-lg border-l-4 border-primary bg-base-200 p-5 shadow-lg ${className}`}
+	class={`flex flex-col items-center justify-center rounded-r-lg border-l-4 border-primary bg-base-200 p-5 shadow-lg lg:basis-1/3 ${className}`}
 	href={data.action}
 	target="_blank"
 	rel="noreferrer"
