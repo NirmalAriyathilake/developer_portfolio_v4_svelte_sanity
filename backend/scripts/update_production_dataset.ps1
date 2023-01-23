5985583e9f48ce08835dc986aa5e3eb2bda8217d
Nirmal Ariyathilake <nirmal.ariyathilake@gmail.com>
2023-01-23 20:09:12 +0530

    Backend : Update scripts

--- backend/scripts/update_production_dataset.ps1
+++ backend/scripts/update_production_dataset.ps1
@@ -1,5 +1,5 @@
 sanity dataset export development ./development.tar.gz
 
-sanity dataset import ./development.tar.gz production
+sanity dataset import ./development.tar.gz production --replace
 
 rm ./development.tar.gz
